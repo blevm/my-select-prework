e84d7028bcac720d57192b3676486e19419a5de1
Michelle Blevins <mjblevins7@gmail.com>
2018-03-24 22:04:32 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,14 @@
 def my_select(collection)
   if block_given?
     i = 0
-    new_array = []
+    new_collection = []
 
-    while i < array.length
-      new_array << yield(array[i])
+    while i < collection.length
+      new_collection << yield(collection[i])
       i = i + 1
     end
 
-    new_array
+    new_collection
   else
     "No block was given!"
   end
